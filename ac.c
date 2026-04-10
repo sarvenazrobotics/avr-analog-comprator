e75554125a37bc7acd14b1850db8bb059e96be71
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-04-10 14:26:57 +0200

    analog comprator

---
+++ ac.c
@@ -0,0 +1,17 @@
+/*
+ * ac.c
+ *
+ * Created: 4/10/2026 2:20:23 PM
+ * Author: Sarve
+ */
+
+#include <io.h>
+
+void main(void)
+{
+while (1)
+    {
+    // Please write your application code here
+
+    }
+}
