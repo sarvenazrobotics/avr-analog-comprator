f71621e8e79792e9b6c26969ae2b4ccbfd5781fe
sarvenazrobotics <sarvenazrobotics@gmail.com>
2026-04-10 15:02:12 +0200

    analog comprator declare

--- ac.c
+++ ac.c
@@ -7,3 +7,11 @@
 #define LED4(x) (x==1 ? (PORTB|=1<<3):(PORTB&=~(1<<3)));
 #define LED5(x) (x==1 ? (PORTB|=1<<4):(PORTB&=~(1<<4)));
 #define LED6(x) (x==1 ? (PORTB|=1<<5):(PORTB&=~(1<<5)));
+
+void main(void)
+{
+    DDRB=0xFF;
+    ADCSRA=0;
+    ADCSRB=(1<<ACME);
+    ACSR=0;
+}
